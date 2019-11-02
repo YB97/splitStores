4ffd8fcd312d564e35fbe3d9ef3d26fdc7b5b9bc
Яна <yana@MacBook-Pro-Ana.Dlink>
2019-11-02 13:59:35 +0300

    jsconfig check

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,16 @@
-import React from "react"
-import { makeStyles, withStyles } from "@material-ui/core/styles"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import IconButton from "@material-ui/core/IconButton"
-import AccountCircle from "@material-ui/icons/AccountCircle"
-import MenuItem from "@material-ui/core/MenuItem"
-import Menu from "@material-ui/core/Menu"
-import { Link } from "react-router-dom"
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { Link } from "react-router-dom";
 
-import { blue, white } from "~v"
-import { URI_TO_APPS } from "~/constants"
-import Logo from "~c/Logo"
-import classes from "./styles.module.scss"
+import { URI_TO_APPS } from "~/constants";
+import Logo from "~c/Logo";
+import classes from "./styles.module.scss";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,39 +18,39 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1,
-    color: white,
+    color: "white",
     fontFamily: "Exo",
     fontSize: "24px",
-    fontWeight: "500"
+    fontWeight: 500
   }
-}))
+}));
 
 const StyledToolbar = withStyles({
   root: {}
-})(Toolbar)
+})(Toolbar);
 
-const canShowNavList = () => {
-  return window.location.pathname !== URI_TO_APPS && auth
-}
+const canShowNavList = auth => {
+  return window.location.pathname !== URI_TO_APPS && auth;
+};
 
 export default function MenuAppBar({ color = "white" }) {
-  const cls = useStyles()
-  const [auth, setAuth] = React.useState(true)
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const open = Boolean(anchorEl)
-  const navList = canShowNavList() && (
+  const cls = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const navList = canShowNavList(auth) && (
     <Link to={URI_TO_APPS} className={`${classes.link} ${color}`}>
       Applications
     </Link>
-  )
+  );
 
   const handleMenu = event => {
-    setAnchorEl(event.currentTarget)
-  }
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClose = () => {
-    setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
   return (
     <div className={cls.root}>
@@ -59,7 +58,7 @@ export default function MenuAppBar({ color = "white" }) {
         <StyledToolbar>
           <div className={classes.logo}>
             {/* Яна тут глянь. Я вот хз норм ли так в пропсе писать  */}
-            <Logo color={color} href={canShowNavList() && URI_TO_APPS} />
+            <Logo color={color} href={canShowNavList(auth) && URI_TO_APPS} />
           </div>
           <div className={classes.nav}>{navList}</div>
           {auth && (
@@ -96,5 +95,5 @@ export default function MenuAppBar({ color = "white" }) {
         </StyledToolbar>
       </AppBar>
     </div>
-  )
+  );
 }
