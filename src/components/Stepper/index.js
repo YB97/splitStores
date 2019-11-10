53992e48cb68b12a1a50d9fcc5e2e159f786b6c2
mchalik <maksimchalik@gmail.com>
2019-11-11 01:24:20 +0300

    start layout for new experiment page

---
+++ src/components/Stepper/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Stepper from "@material-ui/core/Stepper"
+import Step from "@material-ui/core/Step"
+import Steplabel from "@material-ui/core/Steplabel"
+import {withStyles} from "@material-ui/core/styles"
+
+export default function ({
+  bg = "#E3603B",
+  color = "#fff",
+  activeStep,
+  steps,
+  className,
+  alternativeLabel = true
+}) {
+  const StyledStepIconLabel = withStyles({
+    root: {
+      backgroundColor: bg,
+      color,
+      height: "50px",
+      width: "50px",
+      display: "flex",
+      borderRadius: "50%",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "24px",
+      lineHeight: "28px"
+    }
+  })(Steplabel)
+
+  return (
+    <Stepper className={className} activeStep={activeStep} alternativeLabe={alternativeLabel}>
+      {console.log(steps)}
+      {steps.map((step, index) => {
+        return (
+          <Step key={index}>
+            <Steplabel StepIconComponent={StyledStepIconLabel}>
+              {step}
+            </Steplabel>
+          </Step>
+        )
+      })}
+    </Stepper>
+  )
+}
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ export default function Title({ title = "", subtitle = "", color = "" }) {
   return (
     <div className={classes.component}>
       <h2 className={`${classes.title} ${color}`}>{title}</h2>
-      <span className={`${classes.subtitle} ${color}`}>{subtitle}</span>
+      {subtitle && <span className={`${classes.subtitle} ${color}`}>{subtitle}</span>}
     </div>
   )
 }
