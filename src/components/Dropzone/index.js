5566c1c427082ae9c7e1a24dd92a136c32eb4465
Aleksandr Abanin <abaninsash@gmail.com>
2019-11-29 00:25:00 +0300

    added preview

--- src/components/Dropzone/index.js
+++ src/components/Dropzone/index.js
@@ -1,56 +1,86 @@
 import React, { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
+import Preview from './Preview'
+
 import classes from "./dropzone.module.scss";
+const MAX_SIZE_FILE = 8e6;
+const MAX_NUMBER_FILES = 6;
 
 function Dropzone() {
   const [files, setFiles] = useState([]);
-  const [errorText, setError] = useState(false);
+  const [errorText, setError] = useState(null);
+  const [previews, setPreviews] = useState([]);
+
+  const renderPreviews = () => {
+    return previews.map(({ src }) => {
+      <Preview />
+      return <img src={src} alt="" />;
+    });
+  };
 
   let message = files.length
     ? `${files.length} files added`
     : "Drag 'n' drop some files here, or click to select files";
 
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
     if (acceptedFiles.length > 5) {
       acceptedFiles.length = 0;
       setError("Max number files is six");
     }
-    acceptedFiles.forEach(file => {
-      const reader = new FileReader();
-
-      reader.onabort = () => (message = "file reading was aborted");
-      reader.onerror = () => (message = "file reading has failed");
-      reader.onload = () => {
-        setError(false);
-        setFiles(acceptedFiles);
-        // Do whatever you want with the file contents
-        const binaryStr = reader.result;
-        console.log(binaryStr);
-      };
-      reader.readAsArrayBuffer(file);
+    setFiles(acceptedFiles);
+    Promise.all(acceptedFiles.map(uploadFile)).then(data => {
+      setPreviews(data);
     });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <div className={classes.dropzone} {...getRootProps()}>
-      <input {...getInputProps()} />
+      <input
+        {...getInputProps()}
+        size={MAX_SIZE_FILE * MAX_NUMBER_FILES}
+        accept="image/*"
+      />
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
         <>
           <div className={classes.text}>
-            {message}
-            {errorText ? (
-              <span className={classes.error}>{errorText}</span>
+            {previews.length > 0 ? (
+              <div className={classes.previews}>{renderPreviews()}</div>
             ) : null}
+            {message}
+            {errorText && <span className={classes.error}>{errorText}</span>}
           </div>
         </>
       )}
     </div>
   );
+
+  function uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      if (!validateFile(file)) {
+        setError(`file ${file.name} is big. Please, upload another file`);
+      }
+      const reader = new FileReader();
+
+      reader.onabort = () => reject("file reading was aborted");
+      reader.onerror = () => reject("file reading has failed");
+      reader.onload = () => {
+        const binaryStr = reader.result;
+        resolve({ file, src: binaryStr });
+      };
+      reader.readAsDataURL(file);
+    });
+  }
 }
 
 export default Dropzone;
+
+function validateFile(file) {
+  if (file.size > MAX_SIZE_FILE) {
+    return false;
+  }
+  return true;
+}
